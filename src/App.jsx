8938b663df60f384168d1b9b0880c001b90cb193
FractalDevs <andros17@protonmail.com>
2022-12-13 18:48:37 -0500

    process main

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-function App() {
+import { Navigate, Route, Router } from "react-router-dom";
+import Home from "./components/Home";
+import ZonesSelect from "./components/ZonesSelect";
 
+function App() {
   return (
-    <div className="App">
-      
-    </div>
-  )
+    <Router>
+      <Route path="/" element={<ZonesSelect />} />
+      <Route path=":slug" element={<Home />} />
+      <Route path="*" element={<Navigate replace to="/" />} />
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ZonesSelect.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ZonesSelect() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ZonesSelect
