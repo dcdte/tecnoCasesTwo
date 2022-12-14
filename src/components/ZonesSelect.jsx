07f859482bdddb2c643b8ac767f0b9994b18b7fd
DecDev <federico.lopezvi@gmail.com>
2022-12-14 15:44:56 -0500

    creación del textInput

--- src/components/ZonesSelect.jsx
+++ src/components/ZonesSelect.jsx
@@ -1,22 +1,25 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getZonesAsync } from '../store/slices/main/async';
-import {showZones} from './../store/slices/main/selectors';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getZonesAsync } from "../store/slices/main/async";
+import { showZones } from "./../store/slices/main/selectors";
+import TextInput from "./atoms/TextInput";
 
 function ZonesSelect() {
-
   const zones = useSelector(showZones);
   const dispatch = useDispatch();
 
-  useEffect(()=> {
+  useEffect(() => {
     dispatch(getZonesAsync());
-  }, [dispatch])
+  }, [dispatch]);
 
   return (
     <div>
-      {zones.map(item => <p>{item.subzone}</p>)}
+      {/* {zones.map((item) => (
+        <p>{item.subzone}</p>
+      ))} */}
+      <TextInput />
     </div>
-  )
+  );
 }
 
-export default ZonesSelect
+export default ZonesSelect;
---
+++ src/components/atoms/TextInput.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../../styles/css/TextInput.css";
+
+const TextInput = (value, setValue, placeHolder) => {
+
+  /* 
+    TODO Se necesita alojar dentro de un container para asignarle su tamaño
+    TODO Se necesita hacer las funciones de busqueda que ejecutará este componente
+    TODO se necesita traer el placeHolder desde su
+    * * Los estilos están en la carpeta de sass llamado TextInput.css
+    ! * el value se asigna y se queda completamente en el input
+  */ 
+
+  return (
+    <input
+      //value={value}-
+      type="text"
+      placeholder='Marca o Referencia'
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
+};
+
+export default TextInput;
