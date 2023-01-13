93fdac6fa0dbb29212e380afb284c4839bb94106
DecDev <federico.lopezvi@gmail.com>
2023-01-13 10:19:42 -0500

    tonces

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,10 +15,12 @@ import {
 } from "react-icons/md";
 import Label from "./atoms/Label";
 import CardPay from "./atoms/CardPay";
+import Skeleton from "react-loading-skeleton";
 
 function Card({ data }) {
   const [pays, setPays] = useState(data.credits[0]);
   const [zoom, setZoom] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   let renderZoom = () => {
     if (zoom === true) {
@@ -43,71 +45,86 @@ function Card({ data }) {
     }
   };
 
+  const loader = () => {
+    if (loading === true) {
+      return (
+        <article className="card">
+          <div className="card__visual">
+            <Skeleton />
+          </div>
+        </article>
+      );
+    }
+  };
+
   return (
-    <article className="card">
-      <div className="card__visual">
-        <div className="card__img" onClick={() => setZoom(true)}>
-          <motion.img
-            src={small}
-            alt=""
-            whileHover={{ scale: 1.05, transition: { duration: 0.5 } }}
-          />
-        </div>
-      </div>
-      <div className="card__info">
-        <h4 className="card__brand">{data.brand.brand}</h4>
-        <h3 className="card__title">{data.reference}</h3>
-        <div className="card__details">
-          <Label title="Cámara" value={data.camera}>
-            <AiOutlineCamera className="label--container__icon" />
-          </Label>
-          <Label title="Almacenamiento" value={data.storage}>
-            <MdOutlineSdStorage className="label--container__icon" />
-          </Label>
-          <Label title="RAM" value={data.ram}>
-            <MdMemory className="label--container__icon" />
-          </Label>
-          <Label title="Procesador" value={data.processor}>
-            <MdDeveloperBoard className="label--container__icon" />
-          </Label>
-          <Label title="Pantalla" value={data.window}>
-            <MdOutlineScreenshot className="label--container__icon" />
-          </Label>
-          <Label title="Batería" value={data.battery}>
-            <MdBatteryCharging60 className="label--container__icon" />
-          </Label>
-        </div>
-        <div className="card__finances">
-          <span className="card__finance">Financiera:</span>
-          <div className="card__tags">
-            {data.credits &&
-              data.credits.map((item) => (
-                <Tag
-                  id={item.financeId}
-                  hover={false}
-                  isActive={pays.financeId === item.financeId}
-                  handler={() => {
-                    setPays(item);
-                  }}
-                >
-                  {item.finance.name}
-                </Tag>
-              ))}
+    <>
+      <article className="card">
+        <div className="card__visual">
+          <div className="card__img" onClick={() => setZoom(true)}>
+            <motion.img
+              src={small}
+              alt=""
+              whileHover={{ scale: 1.05, transition: { duration: 0.5 } }}
+            />
           </div>
         </div>
-      </div>
-      <div className="card__pays">
-        <div className="card__row">
-          <CardPay title="Inicial" value={pays.initial} />
-          <CardPay title="8 Cuotas" value={pays.eightPays} />
+        <div className="card__info">
+          <h4 className="card__brand">{data.brand.brand}</h4>
+          <h3 className="card__title">{data.reference}</h3>
+          <div className="card__details">
+            <Label title="Cámara" value={data.camera}>
+              <AiOutlineCamera className="label--container__icon" />
+            </Label>
+            <Label title="Almacenamiento" value={data.storage}>
+              <MdOutlineSdStorage className="label--container__icon" />
+            </Label>
+            <Label title="RAM" value={data.ram}>
+              <MdMemory className="label--container__icon" />
+            </Label>
+            <Label title="Procesador" value={data.processor}>
+              <MdDeveloperBoard className="label--container__icon" />
+            </Label>
+            <Label title="Pantalla" value={data.window}>
+              <MdOutlineScreenshot className="label--container__icon" />
+            </Label>
+            <Label title="Batería" value={data.battery}>
+              <MdBatteryCharging60 className="label--container__icon" />
+            </Label>
+          </div>
+          <div className="card__finances">
+            <span className="card__finance">Financiera:</span>
+            <div className="card__tags">
+              {data.credits &&
+                data.credits.map((item) => (
+                  <Tag
+                    id={item.financeId}
+                    hover={false}
+                    isActive={pays.financeId === item.financeId}
+                    handler={() => {
+                      setPays(item);
+                    }}
+                  >
+                    {item.finance.name}
+                  </Tag>
+                ))}
+            </div>
+          </div>
         </div>
-        <div className="card__row">
-          <CardPay title="12 Cuotas" value={pays.twelvePays} />
-          <CardPay title="16 Cuotas" value={pays.sixteenPays} />
+        <div className="card__pays">
+          <div className="card__row">
+            <CardPay title="Inicial" value={pays.initial} />
+            <CardPay title="8 Cuotas" value={pays.eightPays} />
+          </div>
+          <div className="card__row">
+            <CardPay title="12 Cuotas" value={pays.twelvePays} />
+            <CardPay title="16 Cuotas" value={pays.sixteenPays} />
+          </div>
         </div>
-      </div>
-      <AnimatePresence>{renderZoom()}</AnimatePresence>
-    </article>
+        <AnimatePresence>{renderZoom()}</AnimatePresence>
+      </article>
+      {loader()}
+    </>
   );
 }
 
