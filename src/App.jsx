83e2296c074caddaf8f518f7f8dc714738decefc
dcdte <federico.lopezvi@gmail.com>
2023-11-30 12:06:30 -0500

    add Stores

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import UnaHora from "./components/UnaHora";
 import Aliados from "./components/Aliados";
 import { SearchImei } from "./components/SearchImei";
 import { useLocation } from "react-router-dom";
+import { Stores } from "./components/Stores";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path="/supercase" element={<Cases />} />
         <Route path="/ubicacionaliados" element={<Aliados />}></Route>
         <Route path="/imei" element={<SearchImei />}></Route>
+        <Route path="/tiendas" element={<Stores />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Stores.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/css/Stores.css";
+
+export const Stores = () => {
+  return (
+    <div className="stores">
+      <iframe
+        height="100%"
+        width="100%"
+        frameborder="0"
+        allowTransparency="true"
+        scrolling="auto"
+        src="https://creatorapp.zohopublic.com/vt.cel/tecnosuper/report-embed/Tiendas_al_publico/4nttOTMeVMjaMa0yXtwQQbejqWnZySA8xNPwgHQJ0p1k2rCKw0PXrRysPOmjGJGpjGzhUUAu4YSRnCfGXdTp6569MYP2Q8beTNYP"
+      ></iframe>
+    </div>
+  );
+};
